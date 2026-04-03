181389495bf054c418ee4b8c2318c7e612b21e05
Akash Sharma <i.akashhhh@gmail.com>
2026-04-03 15:45:01 +0530

    [Akash] : added Angular Frontend implementation

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:8080',
+  googleClientId: '761220966982-c5h3budhgbjto6q61srjp314vf7mur60.apps.googleusercontent.com'
+};
